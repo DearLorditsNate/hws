f4fb0c6bb425c28df6afd6e71df17fc4d36c91ef
Nate Micinski <nmicinski@gmail.com>
2019-11-03 20:12:29 -0800

    add mobile navbar

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var elems = document.querySelectorAll(".collapsible");
-  var instances = M.Collapsible.init(elems);
+  var menuElems = document.querySelectorAll(".collapsible");
+  var menuInstances = M.Collapsible.init(menuElems);
+
+  var mobileNavElems = document.querySelectorAll(".sidenav");
+  var mobileNavInstances = M.Sidenav.init(mobileNavElems);
 });
 
 console.log("whattup");
